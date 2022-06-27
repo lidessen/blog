ee90f6cdfc52c290eec3ca9083b38f2a88488d11
tianyang.zhou <tianyang.zhou@zoom.us>
2022-06-27 11:58:07 +0800

    update

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
 
 blog({
-  title: "Welcome aoiste's land",
+  title: "Aoiste's land",
   description: "多少事，从来急；天地转，光阴迫。",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
-  author: "aoiste",
+  author: "Aoiste",
   background: "#f9f9f9",
   links: [
     { title: "GitHub", url: "https://github.com/aoiste" },
