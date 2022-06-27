17a9ceefe8f82a305faa194ee70b49d9ea470ff5
tianyang.zhou <tianyang.zhou@zoom.us>
2022-06-27 09:21:09 +0800

    init blog

---
+++ main.ts
@@ -0,0 +1,14 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  title: "aoiste Land",
+  description: "多少事，从来急；天地转，光阴迫。",
+  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  avatarClass: "rounded-full",
+  author: "ztytotoro",
+  background: "#f9f9f9",
+  links: [
+    { title: "Email", url: "ztytotoro@outlook.com" },
+    { title: "GitHub", url: "https://github.com/ztytotoro" },
+  ],
+});
