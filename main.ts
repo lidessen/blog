3eee73d8e5233cf616c40937a5bb7eb140049763
tianyang.zhou <tianyang.zhou@zoom.us>
2022-06-27 11:56:40 +0800

    update

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
 
 blog({
-  title: "aoiste Land",
+  title: "Welcome aoiste's land",
   description: "多少事，从来急；天地转，光阴迫。",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
-  author: "ztytotoro",
+  author: "aoiste",
   background: "#f9f9f9",
   links: [
+    { title: "GitHub", url: "https://github.com/aoiste" },
     { title: "Email", url: "mailto:ztytotoro@outlook.com" },
-    { title: "GitHub", url: "https://github.com/ztytotoro" },
   ],
 });
