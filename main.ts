14029d584a0e84b270e8286733036fbcdc177880
ztytotoro <ztytotoro@outlook.com>
2022-07-31 12:32:54 +0800

    update

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+import blog from "https://deno.land/x/blog@0.4.2/blog.tsx";
 
 blog({
   title: "Aoiste's land",
@@ -6,7 +6,6 @@ blog({
   avatar: "https://deno-avatar.deno.dev/avatar/4bec32.svg",
   avatarClass: "rounded-full",
   author: "Aoiste",
-  background: "#f9f9f9",
   links: [
     { title: "GitHub", url: "https://github.com/aoiste" },
     { title: "Email", url: "mailto:aoiste@outlook.com" },
