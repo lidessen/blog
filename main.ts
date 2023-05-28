7df1eb5a9ffc90651983864a72daa056ddc92f59
Lidessen <8148113+lidessen@users.noreply.github.com>
2023-05-28 15:52:44 +0800

    chore: update

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 import blog from "https://deno.land/x/blog@0.4.2/blog.tsx";
 
 blog({
-  title: "Aoiste's land",
+  title: "Lidessen's land",
   description: "多少事，从来急；天地转，光阴迫。",
   avatar: "https://deno-avatar.deno.dev/avatar/4bec32.svg",
   avatarClass: "rounded-full",
-  author: "Aoiste",
+  author: "Lidessen",
   links: [
-    { title: "GitHub", url: "https://github.com/aoiste" },
-    { title: "Email", url: "mailto:aoiste@outlook.com" },
+    { title: "GitHub", url: "https://github.com/lidessen" },
+    { title: "Email", url: "mailto:lidessen@outlook.com" },
   ],
 });
