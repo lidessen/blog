8834d16b56358714c3eb8ba2df057d5df9f5282e
Lidessen <ztytotoro@outlook.com>
2023-12-09 20:33:04 +0800

    update

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-import blog from "https://deno.land/x/blog@0.4.2/blog.tsx";
+import blog from "https://deno.land/x/blog@0.7.0/blog.tsx";
 
 blog({
   title: "Lidessen's land",
   description: "多少事，从来急；天地转，光阴迫。",
   avatar: "https://deno-avatar.deno.dev/avatar/4bec32.svg",
+  favicon: "https://deno-avatar.deno.dev/avatar/4bec32.svg",
   avatarClass: "rounded-full",
   author: "Lidessen",
   links: [
